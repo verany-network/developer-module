b7418fc7b750f28f53f0c4b1b2f8468d69453ee2
tylix <bytydoo@gmail.com>
2021-03-20 00:00:04 +0100

    Create CheckTask.java

---
+++ developer-parent/src/main/java/net/verany/developer/task/CheckTask.java
@@ -0,0 +1,36 @@
+package net.verany.developer.task;
+
+import com.google.gson.Gson;
+import net.verany.api.module.VeranyProject;
+import net.verany.api.task.AbstractTask;
+import net.verany.developer.DeveloperData;
+import net.verany.developer.ServiceData;
+import net.verany.developer.VeranyDeveloperModule;
+import net.verany.developer.tasks.ServiceTask;
+import org.bson.Document;
+
+public class CheckTask extends AbstractTask {
+
+    private final VeranyProject project;
+
+    public CheckTask(long waitTime, VeranyProject project) {
+        super(waitTime);
+        this.project = project;
+    }
+
+    @Override
+    public void run() {
+        for (Document document : project.getConnection().getCollection("developers").find()) {
+            DeveloperData data = new Gson().fromJson(document.toJson(), DeveloperData.class);
+            ServiceData serviceData = data.getServiceData();
+            if (serviceData == null) continue;
+            for (int i = 0; i < serviceData.getTasks().size(); i++) {
+                ServiceTask task = serviceData.getTasks().get(i);
+                VeranyDeveloperModule.INSTANCE.runTask(data, task);
+            }
+            /*for (ServiceTask task : serviceData.getTasks()) {
+                VeranyDeveloperModule.INSTANCE.runTask(data, task);
+            }*/
+        }
+    }
+}
