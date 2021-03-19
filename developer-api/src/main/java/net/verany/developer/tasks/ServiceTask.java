bd861deac6de0031c5fcd22e5b52d64772c7a0c8
tylix <bytydoo@gmail.com>
2021-03-20 00:00:10 +0100

    Create ServiceTask.java

---
+++ developer-api/src/main/java/net/verany/developer/tasks/ServiceTask.java
@@ -0,0 +1,21 @@
+package net.verany.developer.tasks;
+
+import de.dytanic.cloudnet.driver.service.ServiceInfoSnapshot;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ServiceTask {
+
+    private final TaskType taskType;
+    private final String data;
+
+    public enum TaskType {
+        START,
+        STOP
+    }
+
+}
