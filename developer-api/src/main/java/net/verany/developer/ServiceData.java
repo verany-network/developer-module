684a344156b7d4f42247f2b01b22fe7c86dc0bee
tylix <bytydoo@gmail.com>
2021-03-20 13:18:58 +0100

    Update ServiceData.java

--- developer-api/src/main/java/net/verany/developer/ServiceData.java
+++ developer-api/src/main/java/net/verany/developer/ServiceData.java
@@ -2,26 +2,22 @@ package net.verany.developer;
 
 import de.dytanic.cloudnet.driver.service.ServiceInfoSnapshot;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import net.verany.api.Verany;
 import net.verany.developer.tasks.ServiceTask;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
-@RequiredArgsConstructor
 @Getter
 @Setter
 public class ServiceData {
 
     private final String id = Verany.generate(10);
-    private boolean online;
-    private boolean starting;
-    private boolean staticService;
+    private boolean online = false;
+    private boolean starting = false;
+    private boolean staticService = true;
     private ServiceInfoSnapshot infoSnapshot;
-    private final List<String> plugins = new ArrayList<>();
     private final List<ServiceTask> tasks = new ArrayList<>();
 
 }
