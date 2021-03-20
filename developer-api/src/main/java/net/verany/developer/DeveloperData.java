0b9b6257ca538b095e3f9084f50dc385ab874047
tylix <bytydoo@gmail.com>
2021-03-20 13:19:00 +0100

    Update DeveloperData.java

--- developer-api/src/main/java/net/verany/developer/DeveloperData.java
+++ developer-api/src/main/java/net/verany/developer/DeveloperData.java
@@ -4,14 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+import java.util.UUID;
+
 @AllArgsConstructor
 @Getter
 @Setter
 public class DeveloperData {
 
-    private final String name;
+    private final UUID uniqueId;
     private final boolean hasService;
     private final int memory;
+    private final int maxMemory;
     private ServiceData serviceData;
 
 }
