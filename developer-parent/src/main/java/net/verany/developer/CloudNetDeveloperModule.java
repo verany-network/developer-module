88bf14c77b111ef6ad54031d95cb1ac02b054a3d
tylix <bytydoo@gmail.com>
2021-03-20 13:18:31 +0100

    Update CloudNetDeveloperModule.java

--- developer-parent/src/main/java/net/verany/developer/CloudNetDeveloperModule.java
+++ developer-parent/src/main/java/net/verany/developer/CloudNetDeveloperModule.java
@@ -3,9 +3,9 @@ package net.verany.developer;
 import de.dytanic.cloudnet.driver.module.ModuleLifeCycle;
 import de.dytanic.cloudnet.driver.module.ModuleTask;
 import de.dytanic.cloudnet.module.NodeCloudNetModule;
-import net.verany.api.Verany;
-import net.verany.api.module.VeranyModule;
 import net.verany.api.module.VeranyProject;
+import net.verany.developer.command.SetupCommand;
+import net.verany.developer.listener.CloudNetChannelListener;
 
 public class CloudNetDeveloperModule extends NodeCloudNetModule {
 
@@ -20,6 +20,16 @@ public class CloudNetDeveloperModule extends NodeCloudNetModule {
     public void init() {
         module = new VeranyDeveloperModule();
         module.enable();
+    }
+
+    @ModuleTask(order = 64, event = ModuleLifeCycle.STARTED)
+    public void initListeners() {
+        registerListener(new CloudNetChannelListener());
+    }
+
+    @ModuleTask(order = 60, event = ModuleLifeCycle.STARTED)
+    public void initCommands() {
+        registerCommand(new SetupCommand());
     }
 
     @ModuleTask(order = 35, event = ModuleLifeCycle.STOPPED)
