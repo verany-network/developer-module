b0d09dd6e3549dff052fb7183abecbedd2773abd
tylix <bytydoo@gmail.com>
2021-03-21 22:50:30 +0100

    Update Developer.java

--- developer-bukkit/src/main/java/net/verany/developer/Developer.java
+++ developer-bukkit/src/main/java/net/verany/developer/Developer.java
@@ -3,21 +3,28 @@ package net.verany.developer;
 import de.dytanic.cloudnet.common.document.gson.JsonDocument;
 import de.dytanic.cloudnet.driver.CloudNetDriver;
 import de.dytanic.cloudnet.wrapper.Wrapper;
+import lombok.Getter;
+import lombok.Setter;
 import net.verany.api.Verany;
 import net.verany.api.module.VeranyModule;
 import net.verany.api.module.VeranyProject;
+import net.verany.developer.commands.DevCommand;
 import net.verany.developer.listener.CloudNetChannelListener;
+import net.verany.developer.listener.PlayerQuitListener;
 import net.verany.developer.tasks.UpdateTask;
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.UUID;
 
+@Getter
+@Setter
 @VeranyModule(name = "Developer", maxRounds = -1, prefix = "", version = "2021.3.1", authors = {"tylix"}, user = "tylix", host = "159.69.63.105", password = "RxNqA18HB56SS7GW", databases = {"developer", "network"})
 public class Developer extends VeranyProject {
 
     public static Developer INSTANCE;
 
-    private long lastPlayerOnline = -1;
+    private long lastPlayerOnline = System.currentTimeMillis();
 
     @Override
     public void onEnable() {
@@ -34,6 +41,8 @@ public class Developer extends VeranyProject {
 
         CloudNetDriver.getInstance().getEventManager().registerListener(new CloudNetChannelListener());
 
+        this.getCommand("dev").setExecutor(new DevCommand());
+
         if(getServiceProperties().contains("uuid")) {
             new BukkitRunnable() {
                 @Override
@@ -42,11 +51,16 @@ public class Developer extends VeranyProject {
                 }
             }.runTask(this);
             Verany.addTask(new UpdateTask(1000));
+
+            new PlayerQuitListener(this);
+
+            Bukkit.getWhitelistedPlayers().add(Bukkit.getOfflinePlayer(getServiceProperties().get("uuid", UUID.class)));
         }
     }
 
     @Override
     public void onDisable() {
+        if(getServiceProperties().contains("uuid"))
         CloudNetDriver.getInstance().getMessenger().sendChannelMessage("dev_server", "offline", JsonDocument.newDocument().append("uuid", getServiceProperties().get("uuid", UUID.class)));
         getConnection().disconnect();
     }
