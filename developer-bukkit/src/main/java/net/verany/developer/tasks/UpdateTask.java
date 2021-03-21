fbb80231aa6c0198f201c035a9b33ad7a4f4fe51
tylix <bytydoo@gmail.com>
2021-03-21 22:50:21 +0100

    Update UpdateTask.java

--- developer-bukkit/src/main/java/net/verany/developer/tasks/UpdateTask.java
+++ developer-bukkit/src/main/java/net/verany/developer/tasks/UpdateTask.java
@@ -2,11 +2,14 @@ package net.verany.developer.tasks;
 
 import de.dytanic.cloudnet.common.document.gson.JsonDocument;
 import de.dytanic.cloudnet.driver.CloudNetDriver;
+import de.dytanic.cloudnet.ext.bridge.BridgeServiceProperty;
 import de.dytanic.cloudnet.wrapper.Wrapper;
 import net.verany.api.task.AbstractTask;
 import net.verany.developer.Developer;
+import org.bukkit.Bukkit;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class UpdateTask extends AbstractTask {
 
@@ -16,6 +19,9 @@ public class UpdateTask extends AbstractTask {
 
     @Override
     public void run() {
+        if (Developer.INSTANCE.getLastPlayerOnline() + TimeUnit.MINUTES.toMillis(10) < System.currentTimeMillis())
+            Bukkit.shutdown();
+
         CloudNetDriver.getInstance().getMessenger().sendChannelMessage("dev_server", "serviceInfoSnapshot", JsonDocument.newDocument().append("uuid", Developer.INSTANCE.getServiceProperties().get("uuid", UUID.class)).append("infoSnapshot", Wrapper.getInstance().getCurrentServiceInfoSnapshot()));
     }
 }
